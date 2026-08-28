3d09ac9895b07bf1a2ca4557cee21f1d8fe4b0ef
Javier Ferrán <artworks@javierferran.com>
2026-08-28 12:53:56 +0200

    Fix broken single-line comment in craco.config.js

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -98,7 +98,8 @@ let webpackConfig = {
         ],
       };
 
-      // Add health check plugin to webpack if enabled if (config.enableHealthCheck && healthPluginInstance) { webpackConfig.plugins.push(healthPluginInstance); } // Remove ForkTsCheckerWebpackPlugin (causes ajv/ajv-keywords version conflicts, not needed for a JS-only project) webpackConfig.plugins = webpackConfig.plugins.filter( (plugin) => plugin.constructor.name !== 'ForkTsCheckerWebpackPlugin' ); return webpackConfig;
+      / Add health check plugin to webpack if enabled if (config.enableHealthCheck && healthPluginInstance) { webpackConfig.plugins.push(healthPluginInstance); }
+// Remove ForkTsCheckerWebpackPlugin (causes ajv/ajv-keywords version conflicts, not needed for a JS-only project) webpackConfig.plugins = webpackConfig.plugins.filter( (plugin) => plugin.constructor.name !== 'ForkTsCheckerWebpackPlugin' ); return webpackConfig;
     },
   },
 };
